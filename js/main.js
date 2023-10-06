34e51219a02854c2a5b44195b4145fbaf150baae
ml0045 <125812553+ml0045@users.noreply.github.com>
2023-10-06 16:52:06 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Write your pseduo code first! 
+//Get value in C
+//Convert value to F
+//Display F value
+
+document.querySelector('#convert').addEventListener('click', convert)
+
+function convert() {
+  let temp = document.querySelector('#cel').value
+  temp = temp * 9/5 +32
+  document.querySelector('#outputValue').innerText = temp
+}
+
+document.querySelector('#convert2').addEventListener('click', convert2)
+
+function convert2() {
+  let temp = document.querySelector('#fah').value
+  temp = 5/9 * (temp-32)
+  document.querySelector('#outputValue2').innerText = temp
+}
